a72465901b0fb4e8cf0ef8925586f22265d3834e
something15525 <something15525@gmail.com>
2013-01-28 01:50:24 -0800

    fix file paths for GSM APN and SPN files

--- config/gsm.mk
+++ config/gsm.mk
@@ -19,11 +19,11 @@
 
 # GSM APN list
 PRODUCT_COPY_FILES += \
-    vendor/sjelly/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/sjelly/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # GSM SPN overrides list
 PRODUCT_COPY_FILES += \
-    vendor/sjelly/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+    vendor/sjelly/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
