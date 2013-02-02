a7631cf3fbf8c666103fd1bd58b69646440b77c0
something15525 <something15525@gmail.com>
2013-02-02 13:45:13 -0800

    sjelly: update alarm sound to a louder one

--- config/common.mk
+++ config/common.mk
@@ -60,4 +60,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Girtab.ogg \
     ro.config.notification_sound=Proxima.ogg \
-    ro.config.alarm_alert=Cesium.ogg
+    ro.config.alarm_alert=Hassium.ogg
