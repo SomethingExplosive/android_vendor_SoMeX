88db00c1b67667553a713bf1345cfb7e6b54ee8f
Michael Limb <something15525@gmail.com>
2013-11-10 01:58:36 -0800

    SoMeX: remove legacy bluetooth config

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -20,9 +20,5 @@
 # Inherit SoMeX common file
 $(call inherit-product, vendor/SoMeX/config/common.mk)
 
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
-
 # Bring in all video files
 $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
