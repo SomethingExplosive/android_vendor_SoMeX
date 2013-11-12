a7caf706bf945597af02d70cc128a79c2cd2a2ac
Michael Limb <something15525@gmail.com>
2013-11-11 20:47:05 -0800

    SoMeX: fix typo in telephony.mk
    Well this is embarrassing....

--- config/telephony.mk
+++ config/telephony.mk
@@ -17,11 +17,11 @@
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/SoMeX/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # World SPN overrides list
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+    vendor/SoMeX/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # SIM Toolkit
 PRODUCT_PACKAGES += \
